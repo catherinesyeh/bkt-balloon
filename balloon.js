d40215b29e4c79ba7f4e0daf0d7b210486329bd3
catherinesyeh <cy3@williams.edu>
2019-07-16 21:11:21 -0400

    Overlayed sliders on top of game.

--- balloon.js
+++ balloon.js
@@ -49,3 +49,80 @@ function drift(cloud, start, max, dir) {
     cloud.style.left = pos + "px";
   }
 }
+
+// change P(init)
+function changeP(val, slide) {
+   slide.value = val.innerHTML;
+   updateI(slide.value);
+}
+
+reset();
+
+// display initial values for each parameter
+function reset() {
+   var i = document.getElementById("init");
+   i.value = i.defaultValue;
+   updateI(i.defaultValue);
+
+   var t = document.getElementById("trans");
+   t.value = t.defaultValue;
+   updateT(t.defaultValue);
+
+   var s = document.getElementById("slip");
+   s.value = s.defaultValue;
+   updateS(s.defaultValue);
+
+   var g = document.getElementById("guess");
+   g.value = g.defaultValue;
+   updateG(g.defaultValue);
+}
+
+// update slider values
+// P(init)
+function updateI(val) {
+   document.getElementById("demoI").innerHTML = val;
+   updateProbs();
+}
+
+// P(trans)
+function updateT(val) {
+   document.getElementById("demoT").innerHTML = val;
+   updateProbs();
+}
+
+// P(slip)
+function updateS(val) {
+   document.getElementById("demoS").innerHTML = val;
+   updateProbs();
+}
+
+// P(guess)
+function updateG(val) {
+   document.getElementById("demoG").innerHTML = val;
+   updateProbs();
+}
+
+// update P(learned) and P(correct)
+function updateProbs() {
+   // get the parameter values
+   var i = document.getElementById("init").value,
+       i_c = 1.0 - i,
+       t = document.getElementById("trans").value,
+       t_c = 1.0 - t,
+       s = document.getElementById("slip").value,
+       s_c = 1.0 - s,
+       g = document.getElementById("guess").value,
+       g_c = 1.0 - g;
+
+   // conditional probabilities
+   var r = (i * s_c) / (i * s_c + i_c * g),
+       w = (i * s) / (i * s + i_c * g_c);
+
+   var learnC = r + (1.0 - r) * t,
+       cor = document.getElementById("correct");
+   cor.innerHTML = learnC;
+
+   var learnW =  w + (1.0 - w) * t,
+       wro = document.getElementById("wrong");
+   wro.innerHTML = learnW;
+}
